22972defea0152e83897b4b4a32de4d0c7616554
Lee Cuevas <leecuevas@gmail.com>
2026-02-05 23:54:17 -0700

    Add bundle analyzer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
+import { visualizer } from 'rollup-plugin-visualizer';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -13,6 +14,20 @@ export default defineConfig({
     tailwind({ config: { applyBaseStyles: false } }),
     react(),
   ],
+  vite: {
+    plugins: [
+      ...(process.env.ANALYZE_BUNDLE
+        ? [
+            visualizer({
+              filename: 'dist/bundle-report.html',
+              template: 'treemap',
+              gzipSize: true,
+              brotliSize: true,
+            }),
+          ]
+        : []),
+    ],
+  },
   markdown: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
